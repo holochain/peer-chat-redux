a1aec685e9a1738d983dd4f00f431511e0190f95
Willem Olding <willemolding@gmail.com>
2019-02-16 08:26:30 +1100

    removes old files

--- ui-src/.storybook/utils.js
+++
@@ -1,5 +0,0 @@
-import { withInfo } from "@storybook/addon-info";
-import wInfoStyle from "../src/styles.json";
-export function wInfo (text) {
-  return withInfo({ inline: true, source: false, styles: wInfoStyle, text: text });
-}
