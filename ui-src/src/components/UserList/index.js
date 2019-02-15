093e9783bdab1987a32b297d048e128c03de3d86
Willem Olding <willemolding@gmail.com>
2019-02-16 06:47:58 +1100

    adds agent id to user list

--- ui-src/src/components/UserList/index.js
+++ ui-src/src/components/UserList/index.js
@@ -9,6 +9,7 @@ export const UserList = ({ room, current, users }) => (
       >
         <img src={users[user] ? users[user].avatar_url : ''} />
         <p>{users[user] ? users[user].name : '?'}</p>
+        <h5>&nbsp;| {user && `@${user.substring(0,15)}`}</h5>
       </li>
     ))}
   </ul>
