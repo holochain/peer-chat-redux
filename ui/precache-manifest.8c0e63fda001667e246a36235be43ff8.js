8cd66116f93c646ed3f4c92f242cfcc5406f74cc
Willem Olding <willemolding@gmail.com>
2019-02-16 08:31:59 +1100

    adds dna and ui to repo!

---
+++ ui/precache-manifest.8c0e63fda001667e246a36235be43ff8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "33adc04f99b564433bbf",
+    "url": "/static/js/main.33adc04f.chunk.js"
+  },
+  {
+    "revision": "6e2882c2a3b7bcedb8a1",
+    "url": "/static/js/1.6e2882c2.chunk.js"
+  },
+  {
+    "revision": "33adc04f99b564433bbf",
+    "url": "/static/css/main.708a0a7f.chunk.css"
+  },
+  {
+    "revision": "ff884eee79026297047fe948d231107a",
+    "url": "/index.html"
+  }
+];
