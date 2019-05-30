c529887ec1711d812699dacc8aa06e75e987946b
Philip Beadle <philip.beadle@live.com.au>
2019-05-30 10:46:43 +1000

    Working on bridging to P&P

--- dna-src/zomes/chat/code/src/lib.rs
+++ dna-src/zomes/chat/code/src/lib.rs
@@ -8,11 +8,8 @@ extern crate serde_json;
 #[macro_use]
 extern crate holochain_core_types_derive;
 use hdk::{
-    PUBLIC_TOKEN,
     error::ZomeApiResult,
 };
-
-use serde_json::json;
 
 use hdk::holochain_core_types::{
     hash::HashString,
@@ -38,24 +35,6 @@ define_zome! {
 
     genesis: || {
         {
-            hdk::debug("genesis start")?;
-            let result = hdk::call("holochat-p-p-bridge", "profiles", Address::from(PUBLIC_TOKEN.to_string()), // never mind this for now
-                "register_app",
-                json!({"spec": {
-                  "name": "holochain-basic-chat",
-                  "source_dna": "xxx",
-                  "fields": [{
-                        "name": "handle",
-                        "display_name": "Handle",
-                        "required": true,
-                        "description": "",
-                        "usage": "STORE",
-                        "schema": ""
-                    }]
-                }}).into()
-            );
-            hdk::debug(format!("{:?}", result)).unwrap();
-            hdk::debug("genesis end")?;
             Ok(())
         }
     }
