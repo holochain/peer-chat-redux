d955c440b79658d2939c2679b463e8374514fb21
Philip Beadle <philip.beadle@live.com.au>
2019-07-26 16:31:39 +1000

    Bump

--- config.nix
+++ config.nix
@@ -68,8 +68,8 @@ echo "All finished!!!"
   # the previous version will be scanned/bumped by release scripts
   # the current version is what the release scripts bump *to*
   version = {
-   current = "0.0.6";
-   previous = "0.0.5";
+   current = "0.0.7";
+   previous = "0.0.6";
   };
 
   github = {
