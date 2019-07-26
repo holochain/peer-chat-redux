7a32520859a756e9e4a3eabf1cd6f413bfddf1ec
Philip Beadle <philip.beadle@live.com.au>
2019-07-26 18:45:31 +1000

    Release 0.0.8

--- config.nix
+++ config.nix
@@ -62,14 +62,14 @@ echo "All finished!!!"
   # the commit hash that the release process should target
   # this will always be behind what ends up being deployed
   # the release process needs to add some commits for changelog etc.
-  commit = "d955c440b79658d2939c2679b463e8374514fb21";
+  commit = "bdbc3236b3fbc717f9ff1c0d0dfa2487639582a5";
 
   # the semver for prev and current releases
   # the previous version will be scanned/bumped by release scripts
   # the current version is what the release scripts bump *to*
   version = {
-   current = "0.0.7";
-   previous = "0.0.6";
+   current = "0.0.8";
+   previous = "0.0.7";
   };
 
   github = {
